299e76bb062e45c9fa576703700dfc163494d062
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-10-10 21:12:43 +0545

    I had once grieved, gradually, I became able to withstand everything

---
+++ 7kyu/regex-validate-pin-code.ts
@@ -0,0 +1,22 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN(pin: string): boolean {
+  if (pin.length === 4 || pin.length === 6) {
+    return pin.split("").every((char) => char >= "0" && char <= "9");
+  } else {
+    return false;
+  }
+}
+
+console.log(validatePIN("1")); // false
+console.log(validatePIN("12")); // false
+console.log(validatePIN("123")); // false
+console.log(validatePIN("1234")); // true
+console.log(validatePIN("a234")); // false
