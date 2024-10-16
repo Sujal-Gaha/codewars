b9a21985777e4749062bd6b9ccd5ed2c2f112c83
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-10-16 18:43:01 +0545

    insomnia is the worse

---
+++ 8kyu/convert-a-boolean-to-a-string.ts
@@ -0,0 +1,10 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b: boolean): string {
+  return String(b);
+}
+
+console.log(booleanToString(true)); // "true"
+console.log(booleanToString(false)); // "false"
