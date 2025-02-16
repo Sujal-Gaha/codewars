2687bd444b17b283581f89af21a5bdfc41d1632e
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-02-16 11:42:28 +0545

    deleted all the generated .js files

--- 8kyu/even-or-odd.js
+++
@@ -1,9 +0,0 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-function evenOrOdd(n) {
-    return n % 2 === 0 ? "Even" : "Odd";
-}
-console.log(evenOrOdd(2)); // Even
-console.log(evenOrOdd(7)); // Odd
-console.log(evenOrOdd(-42)); // Even
-console.log(evenOrOdd(-7)); // Odd
-console.log(evenOrOdd(0)); // Even
